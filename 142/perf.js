dec0eb3551273285ea1ada39a04db8dea0c0c2ce
Jon Perl <perl.jonathan@gmail.com>
2020-11-04 11:11:46 -0700

    init

---
+++ 142/perf.js
@@ -0,0 +1,40 @@
+const { chromium } = require("playwright");
+
+(async () => {
+  const timing = {
+    launchBrowser: [],
+    launchContext: [],
+    launchPage: [],
+    close: [],
+    total: [],
+  };
+
+  for (let i = 0; i < 20; i++) {
+    const a = Date.now();
+    const browser = await chromium.launch();
+    timing.launchBrowser.push(Date.now() - a);
+
+    const b = Date.now();
+    const context = await browser.newContext();
+    timing.launchContext.push(Date.now() - b);
+
+    const c = Date.now();
+    await context.newPage();
+    timing.launchPage.push(Date.now() - c);
+
+    const d = Date.now();
+    await browser.close();
+    timing.close.push(Date.now() - d);
+
+    timing.total.push(Date.now() - a);
+  }
+
+  console.log("timing", timing);
+
+  Object.keys(timing).forEach((key) => {
+    const times = timing[key];
+    console.log(
+      `time to ${key}: ${times.reduce((a, b) => a + b) / times.length}`
+    );
+  });
+})();
